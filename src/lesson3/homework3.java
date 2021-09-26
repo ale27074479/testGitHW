c922c214349d663f329dd27474a4f62410e519c7
ale27074479 <ale27074479@yandex.ru>
2021-09-26 11:45:49 +0300

    Create homework3.java

---
+++ src/lesson3/homework3.java
@@ -0,0 +1,26 @@
+package lesson3;
+public class homework3 {
+public static void main(String[] args) {
+    int[] array1 = { 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1 };
+    printArray1("1. Before:", array1);
+    change(array1);
+    printArray1("2. After:", array1) }
+}
+//1 задание
+public static void change(int[] array) {
+    for (int i = 0; i < array.length; i++)
+        if (array[i] == 1) {
+            array[i] = 0;
+        } else {
+            array[i] = 1;
+        }
+}
+
+
+
+//2 задание
+//public static void fillIn(int[] array) {
+      //for (int i = 0; i < array.length; i++)
+  //    array[i] = i + 1;
+ //   }
+   // }
