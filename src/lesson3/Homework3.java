50aa5718323517b98387f2ce4b7d50e6e9959b6d
ale27074479 <ale27074479@yandex.ru>
2021-09-26 23:54:05 +0300

    Update Homework3.java

--- src/lesson3/Homework3.java
+++ src/lesson3/Homework3.java
@@ -12,36 +12,81 @@ public class Homework3 {
         System.out.println("$$$$$$$@@@@@@");
         int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         printArray(array3);
-       doubling(array3);
+        doubling(array3);
         printArray(array3);
-      System.out.println("$$$$$$$@@@@@@");
+        System.out.println("$$$$$$$@@@@@@");
+        int side = 15;
+        int[][] array4 = new int[side][side];
+        crossFill(array4);
+        printArray(аrray4);
+        int[] returnArrayFromMethod = returnArray(7, 777);
+        printArray( returnArrayFromMethod);
+        printArray( array3);
+        System.out.println(" Min: " + findMin(array3));
+        System.out.println(" Max: " + findMax(array3));
     }
 
     //1 задание
     public static void printArray(int[] inputArray) {
         for (int i = 0; i < inputArray.length; i++) {
-        System.out.print(inputArray[i] + " ");
+            System.out.print(inputArray[i] + " ");
         }
         System.out.println();
     }
+
     public static void change(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = 1 - array[i];
         }
     }
+
     //2 задание
     public static void fillIn(int[] array2) {
         for (int i = 0; i < array2.length; i++)
             array2[i] = i + 1;
     }
-
-// 3 задание
-public static void doubling(int[] array){
+    // 3 задание
+public static void doubling(int[] array3){
         for(int i=0;i<array3.length;i++)
-        if(array[i]< 6)
+        if(array3[i]< 6)
         array3[i]*=2;
         }
-}
 //4 задание
-    
+public static void crossFill(int[][] array4) {
+    for (int i = 0; i < array4.length; i++) {
+        array4[i][i] = 1;
+        array4[i][array4.length - 1 - i] = 1;
+    }
+}
+//5 задание
+public static int[] returnArray(int len, int initValue) {
+    int[] tempArray = new int[len];
+
+    for (int i = 0; i < tempArray.length; i++) {
+        tempArray[i] = initValue;
+    }
+    return tempArray;
+}
+//6 задание
+private static int findMin(int[] array) {
+    int min = array[0];
+    for (int i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+    public static int findMax(int[] array3) {
+        int max = array3[0];
+        for (int i = 0; i < array3.length; i++) {
+            if (array3[i] > max) {
+                max = array3[i];
+            }
+        }
+        return max;
+    }
+}
+
+
 
