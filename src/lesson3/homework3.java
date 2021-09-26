830a5bfaf23c351e8e46c09d11e14085b0478270
ale27074479 <ale27074479@yandex.ru>
2021-09-26 12:34:04 +0300

    Update homework3.java

--- src/lesson3/homework3.java
+++ src/lesson3/homework3.java
@@ -1,26 +1,24 @@
 package lesson3;
 public class homework3 {
-public static void main(String[] args) {
-    int[] array1 = { 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1 };
+    public static void main(String[] args) {
+        int[] array1 = {0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1};
 
-}
-//1 задание
-public static void printArray( int[] inputArray) {
-   for (int i = 0; i < inputArray.length; i++) {
-   System.out.print(inputArray[i] + " ");
     }
-   System.out.println();
-}
-public static void printArray(String prefix, int[] inputArray){
-System.out.print(prefix + ":");
-printArray(inputArray);}
-}
-public static void change(int[] array) {
-    array[i] = 1 - array[i];
+
+    //1 задание
+    public static void printArray(int[] inputArray) {
+        for (int i = 0; i < inputArray.length; i++) {
+            System.out.print(inputArray[i] + " ");
         }
+        System.out.println();
+    }
 
-
-
+    public static void change(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 1 - array[i];
+        }
+    }
+}
 
 //2 задание
 //public static void fillIn(int[] array) {
