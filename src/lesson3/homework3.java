072d60c0fc24d6e09669aa372a862c03c9ac7fa4
ale27074479 <ale27074479@yandex.ru>
2021-09-26 11:56:08 +0300

    Update homework3.java

--- src/lesson3/homework3.java
+++ src/lesson3/homework3.java
@@ -4,9 +4,15 @@ public static void main(String[] args) {
     int[] array1 = { 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1 };
     printArray1("1. Before:", array1);
     change(array1);
-    printArray1("2. After:", array1) }
+    printArray1("2. After:", array1); }
 }
 //1 задание
+public static void printArray1( int[] inputArray) {
+   System.out.print(int);
+    for (int i = 0; i < inputArray.length; i++) {
+   System.out.print(inputArray[i] + " ");
+    }
+}
 public static void change(int[] array) {
     for (int i = 0; i < array.length; i++)
         if (array[i] == 1) {
