e2abba2b9c75a48a0ec369c438b607ceef418ab2
ale27074479 <ale27074479@yandex.ru>
2021-09-26 15:43:23 +0300

    Update Homework3.java

--- src/lesson3/Homework3.java
+++ src/lesson3/Homework3.java
@@ -10,34 +10,30 @@ public class Homework3 {
         fillIn(array2);
         printArray(array2);
         System.out.println("$$$$$$$@@@@@@");
-    //    int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-   //     printArray(array3);
- //       doubling(array3);
- //       printArray(array3);
-//        System.out.println("$$$$$$$@@@@@@");
+        int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        printArray(array3);
+       doubling(array3);
+        printArray(array3);
+      System.out.println("$$$$$$$@@@@@@");
     }
 
     //1 задание
     public static void printArray(int[] inputArray) {
         for (int i = 0; i < inputArray.length; i++) {
-            System.out.print(inputArray[i] + " ");
+        System.out.print(inputArray[i] + " ");
         }
         System.out.println();
     }
-
     public static void change(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = 1 - array[i];
         }
     }
-
     //2 задание
     public static void fillIn(int[] array2) {
         for (int i = 0; i < array2.length; i++)
             array2[i] = i + 1;
     }
-}
-
 
 // 3 задание
 public static void doubling(int[] array){
@@ -46,3 +42,6 @@ public static void doubling(int[] array){
         array3[i]*=2;
         }
 }
+//4 задание
+    
+
