a12f8f0bc457ac195dbbd97a8f062cb444889cfb
ale27074479 <ale27074479@yandex.ru>
2021-10-07 22:34:06 +0300

    Update Homework6.java

--- src/LESSON6/Homework6.java
+++ src/LESSON6/Homework6.java
@@ -8,10 +8,10 @@ public class Homework6 {
     String eventName;
     String eventResult;
 
-    Cat cat1 = new Cat("Тучка", 2, 200, 1);
-    Cat cat2 = new Cat("Киара", 5, 400, 2);
-    Dog dog1 = new Dog("Лола", 0.5f, 500, 10);
-    Dog dog2 = new Dog("Лео", 1.5f, 350, 5);
+    Cat cat1 = new Cat("Тучка", 2, 200);
+    Cat cat2 = new Cat("Киара", 5, 400);
+    Dog dog1 = new Dog("Лола", 0.5f, 500);
+    Dog dog2 = new Dog("Лео", 1.5f, 350);
 
     Animals[] animals = {cat1, cat2, dog1, dog2};
 
@@ -27,7 +27,7 @@ public class Homework6 {
         result(nameString, eventName, runLength, eventResult);
 
         int swimResult = animals[i].swim(swimLength);
-        eventName = "проплыть на " + Аnimals[i].getMaxSwim() + " метров попытка проплыть на ";
+        eventName = "проплыть на " + animals[i].getMaxSwim() + " метров попытка проплыть на ";
         eventResult = (swimResult == Animals.SWIM_OK) ? tempWinEvent : tempLossEvent;
 
         if (swimResult == Animals.SWIM_NONE)
@@ -39,6 +39,6 @@ public class Homework6 {
 }
 
     private static void result(String nameAnimal, String event, float eventLength, String resultEvent) {
-        System.out.println(nameAnimal + event + eventLength + " м и" + resultEvent);
+        System.out.println(nameAnimal + event + eventLength + " " + resultEvent);
     }
 }
