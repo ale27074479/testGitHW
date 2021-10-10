7387edc127e834f526dae6820f1814d28576c83a
ale27074479 <ale27074479@yandex.ru>
2021-10-10 11:37:04 +0300

    Create GameMap.java

---
+++ src/LESSON7/GameMap.java
@@ -0,0 +1,12 @@
+package LESSON7;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameMap extends JPanel {
+
+        GameMap() {
+            setBackground(Color.blue);
+        }
+
+    }
