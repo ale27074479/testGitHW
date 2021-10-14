aefa2efbf9e60243127f344daab7fa406e7e5c14
ale27074479 <ale27074479@yandex.ru>
2021-10-14 20:09:06 +0300

    Update AppWindow.java

--- src/LESSON7/AppWindow.java
+++ src/LESSON7/AppWindow.java
@@ -5,31 +5,47 @@ import java.awt.*;
 
 public class AppWindow extends JFrame {
 
-        private int winWidth = 1024;
-        private int winHeight = 550;
-        private int winPositionX = 310;
-        private int winPositionY = 170;
+    private int winWidth = 1024;
+    private int winHeight = 550;
+    private int winPositionX = 310;
+    private int winPositionY = 170;
 
-        private GameMap map;
-        private GuiPanel mainPanel;
+    private GameMap map;
+    private GuiPanel mainPanel;
 
-        AppWindow() {
-            setupWindow();
+    public AppWindow() {
+        setupWindow();
 
-            map = new GameMap();
-            mainPanel = new GuiPanel();
+        mainPanel = new GuiPanel(this);
+        map = new GameMap(this);
 
-            add(mainPanel, BorderLayout.EAST);
-            add(map);
-            setVisible(true);
-        }
-
-        private void setupWindow() {
-            setLocation(winPositionX, winPositionY);
-            setSize(winWidth, winHeight);
-            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            setTitle("GUIApplication");
-            setResizable(false);
-        }
-
+        add(mainPanel, BorderLayout.EAST);
+        add(map);
+        setVisible(true);
     }
+
+    private void setupWindow() {
+        setLocation(winPositionX, winPositionY);
+        setSize(winWidth, winHeight);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("GUIApplication");
+        setResizable(false);
+    }
+
+    void log(String msg) {
+        mainPanel.recordLog(msg);
+    }
+
+    void refreshGuiInfo(GameMap map) {
+        mainPanel.refresh(map);
+    }
+
+    void startSession() {
+        map.startNewGame();
+    }
+
+    void changePositionPlayer(int key) {
+        map.update(key);
+    }
+
+}
