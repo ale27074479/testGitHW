86619b146691e69af53e61f9233412e89c3b9921
ale27074479 <ale27074479@yandex.ru>
2021-10-21 22:11:37 +0300

    Create Fruit.java

---
+++ src/JavaLesson3HomeWorkpart2/Fruit.java
@@ -0,0 +1,13 @@
+package JavaLesson3HomeWorkpart2;
+
+public class Fruit {
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    float getWeight() {
+        return weight;
+    }
+}
