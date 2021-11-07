b14ce31a60f7a6b03954ca2246960fef65e9a6ae
ale27074479 <ale27074479@yandex.ru>
2021-11-07 17:25:34 +0300

    Create Main.java

---
+++ src/JavaLesson6HomeWork/Main.java
@@ -0,0 +1,9 @@
+package JavaLesson6HomeWork;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
+}
